2052a100bdb4f20f25e49c7983ae3f9ba2fc1378
Uzair Mehmood <uzair.mehmood@vidizmo.com>
2022-11-01 03:42:15 -0400

    App Registered in the portal

---
+++ Recording Microsoft Teams Live Session/Models/Authentication.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Graph.Core;
+using System.Web;
+
+namespace Recording_Microsoft_Teams_Live_Session.Models
+{
+    public class Authentication
+    {
+        
+    }
+}
