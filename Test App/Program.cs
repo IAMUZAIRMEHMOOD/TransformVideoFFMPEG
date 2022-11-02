8b53b0fd0a890bf254f4afb802cc8138621edd36
iamuzairmehmood <uzairmehmood480@gmail.com>
2022-11-02 06:57:25 -0400

    added console app

---
+++ Test App/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TransformVideo;
+
+Console.WriteLine("Enter Input File Location:");
+string input = Console.ReadLine();
+Console.WriteLine("Enter Output Path: ");
+string output = Console.ReadLine();
+ffmpeglib FF = new ffmpeglib();
+FF.convert(inputPath: input, outputPath: output);
