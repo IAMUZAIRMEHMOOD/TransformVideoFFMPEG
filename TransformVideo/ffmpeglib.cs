202d5988ddd966b572e785636414a918847c3943
iamuzairmehmood <uzairmehmood480@gmail.com>
2022-11-02 05:54:09 -0400

    added exceptions

--- TransformVideo/ffmpeglib.cs
+++ TransformVideo/ffmpeglib.cs
@@ -13,23 +13,27 @@ namespace TransformVideo
         {
             FFmpeg.SetExecutablesPath(@"C:\Users\uzair.mehmood\Desktop\GTP\TransformVideo\ffmpeg");
         }
-        public async Task<bool> convert(string inputPath,string outputPath)
+        public async Task<bool> convert(string inputPath, string outputPath)
         {
+            try 
+            { 
             outputPath = Path.Combine(outputPath, "converted.mp4");
             var mediaInfo = await FFmpeg.GetMediaInfo(inputPath);
             var videoStream = mediaInfo.VideoStreams.First()
                 .SetCodec(VideoCodec.h264)
                 .SetSize(VideoSize.Hd480);
-            await  FFmpeg.Conversions.New()
+            await FFmpeg.Conversions.New()
                 .AddStream(videoStream)
                 .SetOutput(outputPath)
                 .Start();
-          
+            }
+        
+            catch(Exception e)
+                {
+                     Console.WriteLine(e.Message);
+                }
             return true;
         }
-        CatchBlock(Exception e)
-        { 
         
-        }
     }
 }
