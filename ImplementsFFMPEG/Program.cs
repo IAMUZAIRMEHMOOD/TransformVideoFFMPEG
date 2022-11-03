c98f0384698dc0fbebbcd3802ad3beaf7a9a09b5
iamuzairmehmood <uzairmehmood480@gmail.com>
2022-11-03 06:35:45 -0400

    Added video conversion

--- ImplementsFFMPEG/Program.cs
+++ ImplementsFFMPEG/Program.cs
@@ -2,7 +2,7 @@
 using TransformVideo;
 //-----------------Converting 480------------------
 //Declaring Variables
-//int quality; 
+int quality;
 string input, output;
 //Input Video Location
 Console.WriteLine("Enter Input File Location: ");
@@ -14,42 +14,49 @@ Console.WriteLine("Enter Output Path: ");
 Console.ReadKey();
 output = @"C:\Users\uzair.mehmood\Desktop\ffmpeg\";
 Console.WriteLine(@"C:\Users\uzair.mehmood\Desktop\ffmpeg\");
-//Console.Write("Enter video quality: ");
-//quality = 480;
+//Video Quality
+Console.WriteLine("Enter video quality: ");
+Console.ReadKey();
+quality = 480;
+Console.WriteLine("480p");
 transformvideo Transform = new transformvideo();
-Transform.convertvideo(inputPath: input, outputPath: output);
+Transform.convertvideo(inputPath: input, outputPath: output, vidQuality: quality);
 Console.ReadKey();
 ////-----------------Converting 720------------------
-////Input Video Location
-//Console.Write("Enter Input File Location: ");
-//Console.ReadKey();
-//Console.WriteLine(@"C:\Users\uzair.mehmood\Desktop\ffmpeg\input.mp4");
-//input = @"C:\Users\uzair.mehmood\Desktop\ffmpeg\input.mp4";
-////Output Path 
-//Console.Write("Enter Output Path: ");
-//Console.ReadKey();
-//output = @"C:\Users\uzair.mehmood\Desktop\ffmpeg\";
-//Console.WriteLine(@"C:\Users\uzair.mehmood\Desktop\ffmpeg\");
-//Console.Write("Enter video quality: ");
-//quality = 720;
-//Console.WriteLine("720p");
-//Console.ReadKey();
-//Transform.convertvideo(inputPath: input, outputPath: output, quality);
-//Console.ReadKey();
+//Input Video Location
+Console.WriteLine("Enter Input File Location: ");
+Console.ReadKey();
+Console.WriteLine(@"C:\Users\uzair.mehmood\Desktop\ffmpeg\input.mp4");
+input = @"C:\Users\uzair.mehmood\Desktop\ffmpeg\input.mp4";
+//Output Path 
+Console.WriteLine("Enter Output Path: ");
+Console.ReadKey();
+output = @"C:\Users\uzair.mehmood\Desktop\ffmpeg\";
+Console.WriteLine(@"C:\Users\uzair.mehmood\Desktop\ffmpeg\");
+//Video Quality
+Console.WriteLine("Enter video quality: ");
+Console.ReadKey();
+quality = 720;
+Console.WriteLine("720p");
+transformvideo Transform1 = new transformvideo();
+Transform1.convertvideo(inputPath: input, outputPath: output, vidQuality: quality);
+Console.ReadKey();
 ////-----------------Converting 1080------------------
-////Input Video Location
-//Console.Write("Enter Input File Location: ");
-//Console.ReadKey();
-//Console.WriteLine(@"C:\Users\uzair.mehmood\Desktop\ffmpeg\input.mp4");
-//input = @"C:\Users\uzair.mehmood\Desktop\ffmpeg\input.mp4";
-////Output Path 
-//Console.Write("Enter Output Path: ");
-//Console.ReadKey();
-//output = @"C:\Users\uzair.mehmood\Desktop\ffmpeg\";
-//Console.WriteLine(@"C:\Users\uzair.mehmood\Desktop\ffmpeg\");
-//Console.Write("Enter video quality: ");
-//quality = 1080;
-//Console.WriteLine("1080p");
-//Console.ReadKey();
-//Transform.convertvideo(inputPath: input, outputPath: output, quality);
-//Console.ReadKey();
+//Input Video Location
+Console.WriteLine("Enter Input File Location: ");
+Console.ReadKey();
+Console.WriteLine(@"C:\Users\uzair.mehmood\Desktop\ffmpeg\input.mp4");
+input = @"C:\Users\uzair.mehmood\Desktop\ffmpeg\input.mp4";
+//Output Path 
+Console.WriteLine("Enter Output Path: ");
+Console.ReadKey();
+output = @"C:\Users\uzair.mehmood\Desktop\ffmpeg\";
+Console.WriteLine(@"C:\Users\uzair.mehmood\Desktop\ffmpeg\");
+//Video Quality
+Console.WriteLine("Enter video quality: ");
+Console.ReadKey();
+quality = 1080;
+Console.WriteLine("1080p");
+transformvideo Transform2 = new transformvideo();
+Transform2.convertvideo(inputPath: input, outputPath: output, vidQuality: quality);
+Console.ReadKey();
--- TransformVideo/transformvideo.cs
+++ TransformVideo/transformvideo.cs
@@ -14,40 +14,16 @@ namespace TransformVideo
         {
              FFmpeg.SetExecutablesPath(@"C:\Users\uzair.mehmood\Desktop\ffmpeg");
         }
-        public async Task<bool> convertvideo(string inputPath, string outputPath)
+        public async Task<bool> convertvideo(string inputPath, string outputPath, int vidQuality)
         {
-
-
-            //try
-            //{
-
-            //    outputPath = Path.Combine(outputPath, "convertedto480p.mp4");
-            //    var mediaInfo = await FFmpeg.GetMediaInfo(inputPath);
-            //    var videoStream = mediaInfo.VideoStreams.First()
-            //   .SetCodec(VideoCodec.h264)
-            //   .SetSize(VideoSize.Hd480);
-            //    await FFmpeg.Conversions.New()
-            //   .AddStream(videoStream)
-            //   .SetOutput(outputPath)
-            //   .Start();
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //}
-            //return true;
-
-
-
-
             try
             {
                 //var mediaInfo = await FFmpeg.GetMediaInfo(inputPath);
                 //var videoStream = mediaInfo.VideoStreams.First();
-                //switch (vidQuality)
-                //{
+                switch (vidQuality)
+                {
 
-                //    case 480:
+                    case 480:
                         outputPath = Path.Combine(outputPath, "convertedto480p.mp4");
                         IMediaInfo mediaInfo = await FFmpeg.GetMediaInfo(inputPath);
                         IStream audioStream = mediaInfo.AudioStreams.FirstOrDefault()
@@ -56,38 +32,41 @@ namespace TransformVideo
                        .SetCodec(VideoCodec.h264)
                        .SetSize(VideoSize.Hd480);
                         FFmpeg.Conversions.New()
+                       .AddStream(audioStream, videoStream)
+                       .SetOutput(outputPath)
+                       .Start();
+                        break;
+                    case 720:
+                        outputPath = Path.Combine(outputPath, "convertedto720p.mp4");
+                        mediaInfo = await FFmpeg.GetMediaInfo(inputPath);
+                        audioStream = mediaInfo.AudioStreams.FirstOrDefault()
+                        .SetCodec(AudioCodec.aac);
+                        videoStream = mediaInfo.VideoStreams.FirstOrDefault()
+                       .SetCodec(VideoCodec.h264)
+                       .SetSize(VideoSize.Hd720);
+                        FFmpeg.Conversions.New()
                        .AddStream(audioStream,videoStream)
                        .SetOutput(outputPath)
                        .Start();
-
-                //        break;
-                //    case 720:
-                //        outputPath = Path.Combine(outputPath, "convertedto720p.mp4");
-                //        mediaInfo = await FFmpeg.GetMediaInfo(inputPath);
-                //        videoStream = mediaInfo.VideoStreams.First()
-                //       .SetCodec(VideoCodec.h264)
-                //       .SetSize(VideoSize.Hd720);
-                //        await FFmpeg.Conversions.New()
-                //       .AddStream(videoStream)
-                //       .SetOutput(outputPath)
-                //       .Start();
-                //        break;
-                //    case 1080:
-                //        outputPath = Path.Combine(outputPath, "convertedto1080p.mp4");
-                //        mediaInfo = await FFmpeg.GetMediaInfo(inputPath);
-                //        videoStream = mediaInfo.VideoStreams.First()
-                //       .SetCodec(VideoCodec.h264)
-                //       .SetSize(VideoSize.Hd720);
-                //        await FFmpeg.Conversions.New()
-                //       .AddStream(videoStream)
-                //       .SetOutput(outputPath)
-                //       .Start();
-                //        break;
-                //}
+                         break;
+                    case 1080:
+                        outputPath = Path.Combine(outputPath, "convertedto1080p.mp4");
+                        mediaInfo = await FFmpeg.GetMediaInfo(inputPath);
+                        audioStream = mediaInfo.AudioStreams.FirstOrDefault()
+                        .SetCodec(AudioCodec.aac);
+                        videoStream = mediaInfo.VideoStreams.FirstOrDefault()
+                       .SetCodec(VideoCodec.h264)
+                       .SetSize(VideoSize.Hd1080);
+                        FFmpeg.Conversions.New()
+                       .AddStream(audioStream, videoStream)
+                       .SetOutput(outputPath)
+                       .Start();
+                        break;
+                }
 
 
+            
             }
-
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
